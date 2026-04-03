51aef580504bd745b91866d997e7ce17f2d02e70
Libor <libor26@gmail.com>
2026-04-03 13:15:03 +0200

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import "./index.scss";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <CssBaseline />
+    <App />
+  </StrictMode>,
+);
